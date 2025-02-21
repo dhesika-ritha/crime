279c9ac1d5b10fb84fcee0b94753b0f75b346ba1
dhesika-ritha <ritha4022@gmail.com>
2025-02-21 23:01:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def view_all_users():
         return data
 
 def test():
-        st.image("img2.jpg",width=200)
+        st.image("img2.jpg",width=1000)
 
         st.subheader("Predicts your data in an accurate manner")
 
